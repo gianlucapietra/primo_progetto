7d316643c1665aa7cd23c236eefc78330c743e2a
gianlucapietra <pietragianluca@gmail.com>
2021-10-29 13:15:06 +0200

    livello 3

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,10 +3,12 @@ from django.urls.resolvers import URLPattern
 from prima_app.views import homepage
 from prima_app.views import welcome
 from prima_app.views import lista
+from prima_app.views import ChiSiamo
 
 app_name="prima_app"
 urlpatterns=[
     path('',homepage,name='homepage'),
     path('welcome',welcome,name='welcome'),
     path('lista',lista,name='name'),
+    path('ChiSiamo',ChiSiamo,name='ChiSiamo'),
 ]
