3c0448eef00783719cb96416a29a5e527aa7c183
gianlucapietra <pietragianluca@gmail.com>
2021-10-29 13:06:45 +0200

    livelllo 2

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from prima_app.views import homepage
+from prima_app.views import welcome
+from prima_app.views import lista
 
 app_name="prima_app"
 urlpatterns=[
     path('',homepage,name='homepage'),
+    path('welcome',welcome,name='welcome'),
+    path('lista',lista,name='name'),
 ]
