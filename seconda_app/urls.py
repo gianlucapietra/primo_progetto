030eaddfdf8a5f9e7e8bcba6cdd7a10e9ce02b64
gianlucapietra <pietragianluca@gmail.com>
2021-11-04 12:50:29 +0100

    Livello 7

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from seconda_app.views import es_if
+from seconda_app.views import es_ifelse
 app_name="seconda_app"
 urlpatterns =[
     path('es_if',es_if,name="es_if"),
-
+    path('es_ifelse',es_ifelse,name="es_ifelse"),
 ]
--- seconda_app/views.py
+++ seconda_app/views.py
@@ -5,6 +5,13 @@ def es_if(request):
     context= {
         'var1': 200,
         'var2': 200,
-        'var3': 300
+        'var3': 300,
     }
     return render(request,"es_if.html",context)
+def es_ifelse(request):
+    context= {
+        'var1': 100,
+        'var2': 100.0,
+        'var3': 100.50,
+    }
+    return render(request,"es_ifelse.html",context)
