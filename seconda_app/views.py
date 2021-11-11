c6239786ab989e886681697c9a6a6c2cf8e03018
gianlucapietra <pietragianluca@gmail.com>
2021-11-11 10:41:40 +0100

    Livello 8

--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 def es_if(request):
     context= {
@@ -15,3 +15,11 @@ def es_ifelse(request):
         'var3': 100.50,
     }
     return render(request,"es_ifelse.html",context)
+def es_for(request):
+    context= {
+        'list1':[1,datetime.date(2021,11,11), 'non ti arrendere'],
+        'list2':[1,datetime.date(2021,11,11), 'non ti arrendere'],
+    }
+    return render(request, "es_for.html", context)
+    
+    
