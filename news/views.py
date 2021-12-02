de57d1f0e06f2ce651e12fbc14a995c6c1832b6b
gianlucapietra <92153395+gianlucapietra@users.noreply.github.com>
2021-12-02 11:29:38 +0100

    upd

--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Articolo, Giornalista
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>homepage news!</h1>")
