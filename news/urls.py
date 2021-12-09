1e78870ff832c48807768fdc357ddc175822382e
gianlucapietra <92153395+gianlucapietra@users.noreply.github.com>
2021-12-09 11:03:19 +0100

    update

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import home
+from news.views import home, ArticoloDetailViewCB, GiornalistaDetailViewCB,GiornalistiListView
 app_name='news'
 
 
 urlpatterns=[
     path('',home,name="homeview"),
+   # path("articoli/<int:pk>",articoloDetailView,name="articolo_detail")
+   path("articoli/<int:pk>", ArticoloDetailViewCB.as_view() ,name="articolo_detail"),
+    path("lista_articoli/", ArticoloListViewCB.as_view() ,name="lista_articoli"),
+    path("giornalisti/<int:pk>", GiornalistaDetailViewCB.as_view() ,name="giornalista_detail"),
+    path("lista_giornalisti/", GiornalistiListView.as_view() ,name="lista_giornalisti"),
 ]
